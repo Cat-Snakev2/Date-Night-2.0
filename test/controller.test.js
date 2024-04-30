803f59cba79fe15a40f7cc0d60cbc0742e29831e
Richard Araujo <rcad14@gmail.com>
2024-04-30 16:20:41 -0400

    Added Jest testing for the getActivities middleware function. Used Jest to add a mock api call and mock error handling

---
+++ test/controller.test.js
@@ -0,0 +1,25 @@
+jest.mock('../server/controller', () => ({
+  // mock simulates the api calls
+  getActivities: jest.fn().mockResolvedValue('A romantic dinner in rome'),
+  // expected response from mock api call
+}));
+
+const { getActivities } = require('../server/controller');
+
+describe('getActivities', () => {
+  it('should return a good evening date idea', async () => {
+    // any parameter works because mock simulate the answer
+    const response = await getActivities('city', 'type', 'cravings');
+
+    expect(response).toBe('A romantic dinner in rome');
+  });
+
+  it('should handle errors', async () => {
+    getActivities.mockRejectedValue(new Error('No dates could be found'));
+    try {
+      await getActivities('city', 'type', 'craving');
+    } catch (error) {
+      expect(error).toBeDefined();
+    }
+  });
+});
